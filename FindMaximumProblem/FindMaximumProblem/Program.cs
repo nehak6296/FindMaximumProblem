9a281201e97f3d39a168c8cd79b633729fc6ff2a
nehak6296 <nehak6296@gmail.com>
2021-03-22 20:05:56 +0530

    [Neha]Added:Finding maximum integer number

---
+++ FindMaximumProblem/FindMaximumProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FindMaximumProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            int integerOutput = MaximumNumberCheck.MaximumIntegerNumber(11, 44, 33);
+            Console.WriteLine(integerOutput);
+        }
+    }
+}
